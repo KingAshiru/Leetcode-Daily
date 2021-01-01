c5d8cc254c6118bb96b8e85d6bf304f2e18aac48
Tobi Ashiru <tobiashiru58@gmail.com>
2021-01-01 18:45:18 +0100

    Update Valid-Mountain-Array(LC 941).py

--- Valid-Mountain-Array(LC 941).py
+++ Valid-Mountain-Array(LC 941).py
@@ -1,15 +1,22 @@
 class Solution:
     def validMountainArray(self, arr: List[int]) -> bool:
+        # check for only arrays with length greater than 3
         if len(arr) < 3:
             return False
+        
+        #check for the max element in the array
         peak = max(arr)
+        
+        #return False if the max element is at any end of the array
         if peak == arr[len(arr)-1] or peak == arr[0]:
             return False
         
+        #return False if the left side is not strictly increasing
         for j in range(arr.index(peak)):
             if arr[j] >= arr[j+1]:
                 return False
-        
+            
+        #return False if the right side is not strictly decreasing
         for k in range(arr.index(peak)+1,len(arr)):
             if arr[k-1] <= arr[k]:
                 return False
